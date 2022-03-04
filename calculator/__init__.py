70351b9a42757f3d2459ce716948ab849219f865
souvik <souvikp@iitbhilai.ac.in>
2022-03-04 11:59:06 +0530

    master commit

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -52,5 +52,18 @@ def MUL():
     response = "product = " + str(mul)
     return response
 
+@app.route("/exp", methods=["POST"])
+def EXP():
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    exp = a**b
+    response = "answer = " + str(exp)
+    return response
+
+
+
 if __name__== "__main__":
     app.run()
